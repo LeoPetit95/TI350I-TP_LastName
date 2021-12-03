67e0a50dccae9fa642dc29ab5f36c9238312947e
Léo PETIT <94388352+LeoPetit95@users.noreply.github.com>
2021-12-03 16:07:50 +0100

    Update sonarQube01.java

--- sonarQube01.java
+++ sonarQube01.java
@@ -1,6 +1,5 @@
-
 public class sonarQube01 {
     int addHeader(String stringToAdd, int x, int y){
-        return int.format("Added Header %d %d %d", stringToAdd, x, y);
+        return String.format("Added Header %d %d %d", stringToAdd, x, y);
     }
 }
